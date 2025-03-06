f52fa742ea255e746302e07247f47cdf01619dad
walter.wang <wangren.walter@bytedance.com>
2025-03-06 23:47:48 +0800

    add translucent material for main

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,15 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Navbar />
-      <main enable-xr className="flex-grow bg-gray-50 w-full">
+
+
+      
+      <main
+        className="flex-grow   w-full webmaterial"
+        enable-xr
+         
+      >
+      
         <div className="container mx-auto px-4 max-w-7xl">{children}</div>
       </main>
       <Footer />
