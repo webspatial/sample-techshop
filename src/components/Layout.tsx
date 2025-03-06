403047cceba208464429aa7d0284038f588a61ca
Dexter Yang <dexter.yy@gmail.com>
2025-03-06 23:30:07 +0800

    fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Navbar />
-      <main className="flex-grow bg-gray-50 w-full">
+      <main enable-xr className="flex-grow bg-gray-50 w-full">
         <div className="container mx-auto px-4 max-w-7xl">{children}</div>
       </main>
       <Footer />
